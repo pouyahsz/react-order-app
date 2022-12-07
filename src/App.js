90cf3d8e8a781f1c6f01ffa630e97e73ed7f99f9
pouya <hassanalizadeh@gmail.com>
2022-12-07 16:42:41 +0330

    logic added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
+  return (
+    <CartProvider>
+      {cartIsShown && <div>cart ...</div>}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
